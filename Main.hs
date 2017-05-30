f6972683694a78daa2293f2d111b161de263bc31
Neil Mitchell <ndmitchell@gmail.com>
2017-05-30 08:21:15 +0100

    Switch to a simpler data type

--- Main.hs
+++ Main.hs
@@ -1,5 +1,21 @@
 {-# LANGUAGE RecordWildCards, OverloadedStrings #-}
 
+{-
+data CostCentreTree = CostCentreTree
+    {costCentreNodes :: IntMap CostCenter
+    ,costCentreParents :: IntMap CostCenterNo
+    ,costCentreChildren :: IntMap (Set CostCenter)
+    ,costCenterCallSites :: 
+                    costCentreCallSites :: !containers-0.5.7.1:Data.Map.Base.Map
+                                              (Data.Text.Internal.Text, Data.Text.Internal.Text)
+                                              (containers-0.5.7.1:Data.Set.Base.Set CostCentre),
+                    costCentreAggregate :: !containers-0.5.7.1:Data.Map.Base.Map
+                                              Data.Text.Internal.Text
+                                              (containers-0.5.7.1:Data.Map.Base.Map
+                                                 Data.Text.Internal.Text AggregateCostCentre)}
+        -- Defined in `GHC.Prof.Types'
+-}
+
 import GHC.Prof
 import GHC.Prof.Types
 import Data.List.Extra
@@ -11,40 +27,43 @@ import qualified Data.Map as Map
 import qualified Data.IntMap as IntMap
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as T
+import System.Environment
+
+
+type CC = IntMap.IntMap (Maybe CostCentreNo, CostCentre)
+
+toCC :: CostCentreTree -> CC
+toCC cct = IntMap.map f $ costCentreNodes cct
+    where f x = (IntMap.lookup (costCentreNo x) (costCentreParents cct), x)
 
 
 main :: IO ()
 main = do
-    Right Profile{..} <- decode <$> T.readFile "C:/Neil/hlint/hlint.prof"
-    let cc = deleteZeros profileCostCentreTree
-    let root = rootCostCentre cc
-    putStr $ unlines $ map (\(lbl,t) -> show t ++ " " ++ lbl) $
+    args <- getArgs
+    Right prf <- decode <$> T.readFile (head args)
+    let cc = deleteZeros $ toCC $ profileCostCentreTree prf
+    let same = alwaysSameParent cc
+    putStr $ unlines $ take 25 $ map (\(lbl,t) -> show t ++ " " ++ lbl) $
         sortOn (negate . snd) $ map (second sum) $ groupSort
         [ (T.unpack $ costCentreId v, costCentreInhTime v)
-        | (k,v) <- IntMap.toList $ costCentreNodes cc
-        , root k]
+        | (k,(_,v)) <- IntMap.toList cc
+        , not $ same k]
 
 costCentreId x = costCentreModule x <> " " <> costCentreName x
 
--- | Find all the cost centers which should be roots (they don't have exactly 1 parent)
-rootCostCentre :: CostCentreTree -> (CostCentreNo -> Bool)
-rootCostCentre cc = flip Set.notMember singleParent
-    where
-        singleParent = Set.fromList $ concat
-            [ map costCentreNo xs
-            | xs <- map Set.toList $ Map.elems $ costCentreCallSites cc
-            , let parents = nubOrd $ map (costCentreId . (costCentreNodes cc IntMap.!)) $ mapMaybe (flip IntMap.lookup (costCentreParents cc) . costCentreNo) xs
-            , length parents == 1]
+
+-- | Does this cost center and all others with the same Id always
+--   have a parent with the same Id
+alwaysSameParent :: CC -> (CostCentreNo -> Bool)
+alwaysSameParent cc = flip Set.member $
+    Set.unions $ map snd $
+    filter ((==) 1 . Set.size . fst) $
+    Map.elems $ Map.fromListWith (<>)
+    [ (costCentreId v, (Set.singleton $ costCentreId . snd . (cc IntMap.!) <$> p , Set.singleton k))
+    | (k,(p,v)) <- IntMap.toList cc]
 
 
-deleteZeros :: CostCentreTree -> CostCentreTree
-deleteZeros CostCentreTree{..} = CostCentreTree nodes parents children callsites Map.empty
-    where
-        isZero CostCentre{..} = costCentreInhTime == 0 && costCentreInhAlloc == 0
-        goodId x = IntMap.member x nodes
-        good = goodId . costCentreNo
-
-        nodes = IntMap.filter (not . isZero) costCentreNodes
-        parents = IntMap.filterWithKey (\k v -> goodId k && goodId v) costCentreParents
-        children = IntMap.filter (not . Set.null) $ IntMap.map (Set.filter good) costCentreChildren
-        callsites = Map.filter (not . Set.null) $ Map.map (Set.filter good) costCentreCallSites
+-- | Delete cost centers which have zero runtime
+deleteZeros :: CC -> CC
+deleteZeros = IntMap.filter (not . isZero . snd)
+    where isZero CostCentre{..} = costCentreInhTime == 0 && costCentreInhAlloc == 0
